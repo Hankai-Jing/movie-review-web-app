20ae7134731d1d5b811678730e4e976610d7bbae
Jacqueline Guo <jacquelineguo@Jacquelines-MacBook-Pro.local>
2021-12-14 20:13:12 -0800

    add privacy

--- src/components/PrivacyPolicy/index.js
+++ src/components/PrivacyPolicy/index.js
@@ -7,12 +7,22 @@ const PrivacyScreen = () => {
   console.log("profile");
   return(
       <div>
-        <Banner/>
+        <div className="bg-secondary bg-gradient banner text-center fs-1 ">
+          Privacy Policy
+        </div>
         <div className="row mt-2">
           <div className="col-2 col-md-2">
             <NavigationSidebar active="home"/>
           </div>
           <div className="col-10 col-md-10" style={{"position": "relative"}}>
+            When you create a Rotten Potato Account, you provide us with personal information that includes your username and a password. You can also choose to add a email address or your name to your account. We collect these information to personalize your experience. Your username and reviews will be made public to others.
+            <br/>
+            <br/>
+            We also collect the content you create, upload, or receive from others when using our services. This includes things like movie reviews, movies you searched/added. We will be displaying content you generate.
+            <br/>
+            <br/>
+            When you search/add movie, We may receive and store log data that include your search history. The search results comes from a third party API and we do not share any user information except the searched content.
+
           </div>
         </div>
         <Footer/>
